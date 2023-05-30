8ce95e9ae8ef00d4bc7d57aa53933ca39ecb2e47
monority <chenu.pro@gmail.com>
2023-05-30 07:59:54 +0200

    last

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,27 +1,35 @@
 import React, { Component } from "react";
+import { Modal } from "./Modal"
 
 export default class MemberDetail extends Component {
   state = {
     movies: {},
+    companies: {},
+
   };
 
   async getMovies(id) {
-    const data = await fetch("https://elorri.fr/api/disney-plus/movies").then(
-        (response) => response.json());
+    const data = await fetch(`https://elorri.fr/api/disney-plus/movie/${id}`).then(
+      (response) => response.json());
     this.setState({
       movies: data,
     });
   }
+
+
   componentDidMount() {
-    this.getMovies(this.props.match.params);
+    this.getMovies(this.props.match.params.id);
   }
 
   render() {
     return (
       <div>
-        <h1>Film n°{this.props.match.params.id}</h1>
-        <img src={this.props.match.params.cover} alt="" />
-        <p>{this.props.match.params.description}</p>
+
+        <iframe width="420" height="315"
+          src={this.state.movies.video}>
+        </iframe>
+        <p>{this.state.movies.description}</p>
+        <p>{this.state.movies.company}</p>
       </div>
     );
   }
